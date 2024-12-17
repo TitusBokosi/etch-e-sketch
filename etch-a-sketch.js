611da9441e24bda5663510a743a7662ac1e5304c
TitusBokosi <titusbokosi@gmail.com>
2024-12-17 14:08:08 +0200

    added the change number of squares button

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,16 +1,24 @@
 const mainDiv = document.querySelector('div');
-function changeColor(event){
-    let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-    event.target.style.backgroundColor = randomColor;
-}
 
- for( i =0; i < 1000; i++ ){
-    const square = document.createElement('div');
-    square.addEventListener('mouseover', changeColor);
-    mainDiv.appendChild(square);
- }
-
+ 
 const btn = document.createElement('button');
 btn.textContent = 'select the number of squares';
 document.body.insertBefore(btn, mainDiv);
 btn.className = 'btn';
+btn.addEventListener('click', resize);
+
+function resize(){
+   let x = prompt('enter the number of squares');
+
+   for( i =0; i < x; i++ ){
+      const square = document.createElement('div');
+      square.addEventListener('mouseover', changeColor);
+      mainDiv.appendChild(square);
+   }
+  
+}
+
+function changeColor(event){
+   let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+   event.target.style.backgroundColor = randomColor;
+}
